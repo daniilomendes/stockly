4e56a8f8edc884074e7030a8d33ca54e6b231171
danilo <danilo.mirandha@gmail.com>
2024-10-29 19:39:44 -0300

    feat: ensure delete product action uses next-safe-action

--- app/products/_components/delete-dialog.tsx
+++ app/products/_components/delete-dialog.tsx
@@ -8,6 +8,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/app/_components/ui/alert-dialog";
+import { useAction } from "next-safe-action/hooks";
 import { toast } from "sonner";
 
 interface DeleteProductDialogContentProps {
@@ -15,15 +16,16 @@ interface DeleteProductDialogContentProps {
 }
 
 const DeleteDialog = ({ productId }: DeleteProductDialogContentProps) => {
-  const handleContinueClick = async () => {
-    try {
-      await deleteProduct({ id: productId });
+  const { execute: executeDeleteProduct } = useAction(deleteProduct, {
+    onSuccess: () => {
       toast.success("Produto excluído com sucesso!");
-    } catch (error) {
-      toast.success("Ocorreu um erro ao excluir o produto!");
-      console.error(error);
-    }
-  };
+    },
+    onError: () => {
+      toast.error("Ocorreu um erro ao excluír o produto!");
+    },
+  });
+
+  const handleContinueClick = () => executeDeleteProduct({ id: productId });
 
   return (
     <AlertDialogContent>
