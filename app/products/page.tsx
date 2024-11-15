afe7233ab5965cbe1b7c587109d8410c804cd0fa
danilo <danilo.mirandha@gmail.com>
2024-11-15 14:58:07 -0300

    refactor: add header component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,26 +1,29 @@
 import { DataTable } from "../_components/ui/data-table";
 import { productTableColumns } from "./_components/table-columns";
-import {
-  // cacheGetProducts,
-  getProducts,
-} from "../_data-access/product/get-product";
+import { getProducts } from "../_data-access/product/get-product";
 import CreateProductButton from "./_components/create-product-button";
+import Header, {
+  HeaderLeft,
+  HeaderRigth,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "../_components/header";
 
 const ProductsPage = async () => {
   const products = await getProducts();
 
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
-      <div className="flex w-full items-center justify-between">
-        <div className="space-y-1">
-          <span className="text-xs font-semibold text-slate-500">
-            Gestão de produtos
-          </span>
-          <h2 className="text-xl font-semibold">Produtos</h2>
-        </div>
+      <Header>
+        <HeaderLeft>
+          <HeaderSubtitle>Gestão de produtos</HeaderSubtitle>
+          <HeaderTitle>Produtos</HeaderTitle>
+        </HeaderLeft>
 
-        <CreateProductButton />
-      </div>
+        <HeaderRigth>
+          <CreateProductButton />
+        </HeaderRigth>
+      </Header>
 
       <DataTable columns={productTableColumns} data={products} />
     </div>
--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,3 +1,9 @@
+import Header, {
+  HeaderLeft,
+  HeaderRigth,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "../_components/header";
 import { ComboboxOption } from "../_components/ui/combobox";
 import { DataTable } from "../_components/ui/data-table";
 import { getProducts } from "../_data-access/product/get-product";
@@ -21,16 +27,19 @@ const SalesPage = async () => {
 
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
-      <div className="flex w-full items-center justify-between">
-        <div className="space-y-1">
-          <span className="text-xs font-semibold text-slate-500">
-            Gestão de vendas
-          </span>
-          <h2 className="text-xl font-semibold">Vendas</h2>
-        </div>
+      <Header>
+        <HeaderLeft>
+          <HeaderSubtitle>Gestão de vendas</HeaderSubtitle>
+          <HeaderTitle>Vendas</HeaderTitle>
+        </HeaderLeft>
 
-        <UpsertSaleButton products={products} productOptions={productOptions} />
-      </div>
+        <HeaderRigth>
+          <UpsertSaleButton
+            products={products}
+            productOptions={productOptions}
+          />
+        </HeaderRigth>
+      </Header>
 
       <DataTable columns={saleTableColumns} data={tableData} />
     </div>
